77835fb673aa00243e0787b9bea65a8d4677bae3
Haniel Lohn <hanielohn2016@gmail.com>
2022-08-10 22:01:21 -0300

    update

--- lib/views/tela_inicial.dart
+++ lib/views/tela_inicial.dart
@@ -7,10 +7,12 @@ class TelaInicial extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          leading: const Icon(Icons.diamond_outlined),
           title: const Text('Sou Rico'),
+          centerTitle: true,
           backgroundColor: Colors.blueGrey[900],
         ),
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Color.fromARGB(255, 67, 141, 179),
         body: const Center(
           child: Image(
             image: AssetImage(
